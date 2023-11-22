8f16ae0429831ccd5fdf15612a11209666218e91
Thapelo Sean <thapelomatji12@gmail.com>
2023-11-22 14:10:36 +0200

    Modified NumfinderMain class

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -6,10 +6,8 @@ public class NumFinderMain {
         NumFinder nf = new NumFinder();
 
         // this works
-        // nf.find(new int[] {4, 25, 7, 9});
-
-        // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new int[] {0, 25, 7, }); //omitted 3rd index
+        nf.find(new int[] {4, 3, 2, -1});
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());

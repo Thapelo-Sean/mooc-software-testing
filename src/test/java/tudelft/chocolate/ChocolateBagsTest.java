ebb4d0bd31c05c4d8f1db553821cd923179d922d
Thapelo Sean <thapelomatji12@gmail.com>
2023-11-28 09:32:27 +0200

    Modified ChocolateBags class

--- src/test/java/tudelft/chocolate/ChocolateBagsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class ChocolateBagsTest {
-    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1" })
-    public void totalIsTooBig(int small, int big, int total, int expectedResult) {
+
+    @ParameterizedTest(name = "small = {0}, big = {1}, total = {2}, result = {3}")
+    @CsvSource({"5,3,15,0", "1,5,16,1", "5,1,11,-1", "3,2,15,-1"})
+    public void totalTooBig(int small, int big, int total, int expectedResult){
         ChocolateBags bags = new ChocolateBags();
         int result = bags.calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);

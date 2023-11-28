a417751f591599fd980a98d04f6bbf6771076024
Thapelo Sean <thapelomatji12@gmail.com>
2023-11-28 08:11:50 +0200

    Modified CHristmasDiscountRefactoredTest class

--- src/test/java/tudelft/christmas/ChristmasDiscountRefactoredTest.java
+++ src/test/java/tudelft/christmas/ChristmasDiscountRefactoredTest.java
@@ -3,7 +3,6 @@ package tudelft.christmas;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -43,5 +42,14 @@ public class ChristmasDiscountRefactoredTest {
         Mockito.when(clock.now()).thenReturn(christmas);
         double finalValue = new ChristmasDiscountRefactored(clock).applyDiscount(100.0);
         Assertions.assertEquals(100.0, finalValue, 0.0001);
+    }
+
+    @Test
+    public void notChristmasWithNumberZero(){
+        Clock clock = Mockito.mock(Clock.class);
+        Calendar christmas = new GregorianCalendar(2013, Calendar.JUNE, 23);
+        Mockito.when(clock.now()).thenReturn(christmas);
+        double finalValue = new ChristmasDiscountRefactored(clock).applyDiscount(0);
+        Assertions.assertEquals(0,finalValue, 0.0001 );
     }
 }

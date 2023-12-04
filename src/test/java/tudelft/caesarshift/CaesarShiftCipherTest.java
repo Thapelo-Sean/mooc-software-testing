a7c7ad2d7b6b610b93c6ce2bb768681c0b9df20e
Thapelo Sean <thapelomatji12@gmail.com>
2023-12-04 13:55:09 +0200

    Modified CaesarShiftCipherTest class

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,38 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class CaesarShiftCipherTest {
+    @Test
+    public void positiveShiftMultipleCharacters(){
+        CaesarShiftCipher cs = new CaesarShiftCipher();
+        String actualResult = cs.CaesarShiftCipher("abc", 3);
+        String expectedResult = "def";
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void negativeShiftMultipleCharacters(){
+        CaesarShiftCipher cs = new CaesarShiftCipher();
+        String actualResult = cs.CaesarShiftCipher("xyz", -3);
+        String expectedResult = "uvw";
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void positiveShiftSingleCharacter(){
+        CaesarShiftCipher cs = new CaesarShiftCipher();
+        String actualResult = cs.CaesarShiftCipher("a", 1);
+        String expectedResult = "b";
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void negativeShiftOneCharacter(){
+        CaesarShiftCipher cs = new CaesarShiftCipher();
+        String actualResult = cs.CaesarShiftCipher("a", -1);
+        String expectedResult = "z";
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 }

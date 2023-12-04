100ef8ea66b480cb230ad550437a267153d57b6f
Thapelo Sean <thapelomatji12@gmail.com>
2023-12-04 14:04:10 +0200

    Modified GHappyTest.java class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,49 @@
 package tudelft.ghappy;
 
+import org.jugit nit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    @Test
+    public void noGcharacter(){
+        GHappy gHappy = new GHappy();
+        boolean input = gHappy.gHappy("xxxxxx");
+        Assertions.assertEquals(false, input);
+    }
+
+    @Test
+    public void singleGcharacter(){
+        GHappy gHappy = new GHappy();
+        boolean input = gHappy.gHappy("xxgxxx");
+        Assertions.assertEquals(false, input);
+    }
+
+    @Test
+    public void doubleGcharacter(){
+        GHappy gHappy = new GHappy();
+        boolean input = gHappy.gHappy("xxggxx");
+        Assertions.assertEquals(true, input);
+    }
+
+    @Test
+    public void multipleGcharacters(){
+        GHappy gHappy = new GHappy();
+        boolean input = gHappy.gHappy("xxggyyggxx");
+        Assertions.assertEquals(true, input);
+    }
+
+    @Test
+    public void separatedGcharacters(){
+        GHappy gHappy = new GHappy();
+        boolean input = gHappy.gHappy("xxggyygxx");
+        Assertions.assertEquals(false, input);
+    }
+
+    @Test
+    public void onlyGcharacter(){
+        GHappy gHappy = new GHappy();
+        boolean input = gHappy.gHappy("gggggg");
+        Assertions.assertEquals(true, input);
+    }
 }

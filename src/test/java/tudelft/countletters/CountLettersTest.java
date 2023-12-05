6deb4b03816f47a2380a241e690f6ebef703f12b
Thapelo Sean <thapelomatji12@gmail.com>
2023-12-05 14:16:47 +0200

    Modified CountLettersTest.java class

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -7,15 +7,25 @@ public class CountLettersTest {
 
     @Test
     public void twoMatchingWords(){
-        int words = new CountLetters().count("car | cars");
+        int words = new CountLetters().count("programmer | programs");
         Assertions.assertEquals(2,words);
     }
 
-
     @Test
-    public void lastWordDoesNotMatch() {
-        int words = new CountLetters().count("cats|dog");
+    public void mismatchedLastWord(){
+        int words = new CountLetters().count("languages | java");
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    public void mismatchedWords(){
+        int words = new CountLetters().count("javascript | main");
+        Assertions.assertEquals(0, words);
+    }
+
+    @Test
+    public void mismatchedFirstWord(){
+        int words = new CountLetters().count("maven | regex");
+        Assertions.assertEquals(1, words);
+    }
 }

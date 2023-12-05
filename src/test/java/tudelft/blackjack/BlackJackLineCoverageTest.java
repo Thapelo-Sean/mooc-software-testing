8589c2b0c593b34dcf90ee19d813b8f54e87f868
Thapelo Sean <thapelomatji12@gmail.com>
2023-12-05 09:28:05 +0200

    Modified BlackJackLineCoverageTest.java class

--- src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
+++ src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
@@ -6,14 +6,8 @@ import org.junit.jupiter.api.Test;
 public class BlackJackLineCoverageTest {
 
     @Test
-    public void bothPlayersGoTooHigh() {
-        int result = new BlackJack().play(30, 30);
+    public void playersGoTooHigh(){
+        int result = new BlackJack().play(23,22);
         Assertions.assertEquals(0, result);
-    }
-
-    @Test
-    public void leftPlayerWins() {
-        int result = new BlackJack().play(10, 9);
-        Assertions.assertEquals(10, result);
     }
 }

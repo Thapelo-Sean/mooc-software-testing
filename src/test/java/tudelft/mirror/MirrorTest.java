964fd9759cd34a27b6d65edd0aa9d3b1c6929706
Thapelo Sean <thapelomatji12@gmail.com>
2023-12-08 15:50:42 +0200

    Modified MirrorTest.java class

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,41 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void mirrorWithEmptyString(){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("");
+        Assertions.assertEquals("", result);
+    }
+
+    @Test
+    public void mirrorWithSingleLetterOutput(){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("reddar");
+        Assertions.assertEquals("r",result);
+    }
+
+    @Test
+    public void mirrorWithDoubleLetterOutput(){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("deffied");
+        Assertions.assertEquals("de",result);
+    }
+
+    @Test
+    public void mirrorWithTripleLetterOutput(){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("spoops");
+        Assertions.assertEquals("spo", result);
+    }
+     @Test
+    public void mirrorWithUnmatchedLetters(){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("abccbd");
+        Assertions.assertEquals("", result);
+    }
 }
